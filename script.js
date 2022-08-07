dcd289e1572595ac8f1cb36842d4c03b75a4904a
Denys <denishomov2015@gmail.com>
2022-08-07 15:16:44 +0300

    Update

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 $('.slider').slick({
     arrows:false,
     dots:true,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    autoplay:true,
+    autoplaySpeed:10000,
+    responsive:[
+        {
+            
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    autoplay:true,
+                    autoplaySpeed:10000,
+
+                }
+            
+        }
+    ]
 });
 
