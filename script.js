779dabd27162bfa7e6b7586dadecb4a8759b18c9
Denys <denishomov2015@gmail.com>
2022-08-16 10:24:17 +0300

    first bugfix

--- script.js
+++ script.js
@@ -1,34 +1,30 @@
-$(document).ready(function() {
-    $('.header__burger').click(function(event) {
-        $('.header__burger ,.nav').toggleClass('active');
-        $('.logo').toggleClass('logo_active');
+$(document).ready(function () {
+  $(".header__burger").click(function (event) {
+    $(".header__burger ,.nav").toggleClass("active");
+    $(".logo").toggleClass("logo_active");
 
-        $('body').toggleClass('lock');
-    });
+    $("body").toggleClass("lock");
+  });
 });
 
-$('.slider').slick({
-    arrows:false,
-    dots:true,
-    slidesToShow: 2,
-    slidesToScroll: 2,
-    autoplay:true,
-    autoplaySpeed:10000,
-    responsive:[
-        {
-            
-                breakpoint: 767,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true,
-                    autoplay:true,
-                    autoplaySpeed:10000,
-
-                }
-            
-        }
-    ]
+$(".slider").slick({
+  arrows: false,
+  dots: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 10000,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 10000,
+      },
+    },
+  ],
 });
-
